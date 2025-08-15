38e4ad612dfe5b75465327abadba61bfe1418dda
Daniel Muhia <muhiadan17@gmail.com>
2025-08-15 13:57:18 +0300

    completes lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Argument must be an instance of Pet")
+        pet.owner = self
+        if pet not in self._pets:
+            self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = None
+        if owner is not None:
+            if not isinstance(owner, Owner):
+                raise Exception("Owner must be an instance of Owner")
+            owner.add_pet(self)
+        Pet.all.append(self)
